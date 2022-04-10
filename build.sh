2cc2daca33114d58b7d7d93aaf99c866120440c2
Abidin Kiremitci <abidinkiremitci@gmail.com>
2022-04-10 18:06:05 +0200

    Sample Docker build and deploy - spotify and google plugin

--- build.sh
+++ build.sh
@@ -12,6 +12,12 @@ IMAGE_NAME=$PREFIX/$NAME
 pack config default-builder paketobuildpacks/builder:base
 pack build $IMAGE_NAME --path . -e BP_JVM_VERSION=17
 
+## with jib plugin
+#mvn -DskipTests clean package jib:dockerBuild -Dimage.name=$IMAGE_NAME
+
+## with spotify plugin
+#mvn -DskipTests clean package dockerfile:build -Dimage.name=$IMAGE_NAME
+
 ## Running the image
 docker run -p 8088:8088 -e SERVER_PORT=8088 $IMAGE_NAME
 
